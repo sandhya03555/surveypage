c7b1ceaec6f3528101553f759c71b679e40ae074
sandhya03555 <sweetyyadav0000@gmail.com>
2020-08-21 22:32:23 +0530

    remodify

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import './Landing_Page.css';
-import {Link} from 'react-router-dom';
-const LandingPage=(props)=>{
-    return(
-        <div>
-        <div>
+import React from "react";
+import "./Landing_Page.css";
+import { Link } from "react-router-dom";
+const LandingPage = (props) => {
+  return (
+    <div>
+      <div>
         <Link to="/create">
-        <button className="landing-page-button">Create Survey</button>
+          <button className="landing-page-button">Create Survey</button>
         </Link>
-        </div>
-        <div>
+      </div>
+      <div>
         <button className="landing-page-button">Take Survey</button>
-        </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default LandingPage;
+//....
